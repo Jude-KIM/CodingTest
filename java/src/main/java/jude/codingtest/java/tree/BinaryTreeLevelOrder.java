ba030467f9aa4455551269457d5ab4f3d40e698f
JUDE <jude0709@gmail.com>
2019-08-07 10:01:59 +0900

    tree traverse level order

---
+++ java/src/main/java/jude/codingtest/java/tree/BinaryTreeLevelOrder.java
@@ -0,0 +1,66 @@
+package jude.codingtest.java.tree;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BinaryTreeLevelOrder {
+
+    /**
+     * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+     *
+     * For example:
+     * Given binary tree [3,9,20,null,null,15,7],
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * return its level order traversal as:
+     * [
+     *   [3],
+     *   [9,20],
+     *   [15,7]
+     * ]
+     */
+
+    public static void main(String[] args) throws IOException {
+        BinaryTreeInorder.TreeNode node5 = new BinaryTreeInorder.TreeNode(3);
+        node5.left = new BinaryTreeInorder.TreeNode(9);
+//        node5.right = new BinaryTreeInorder.TreeNode(20);
+        BinaryTreeInorder.TreeNode node4 = new BinaryTreeInorder.TreeNode(20);
+        node4.left = new BinaryTreeInorder.TreeNode(15);
+        node4.right = new BinaryTreeInorder.TreeNode(7);
+        node5.right = node4;
+        System.out.println(levelOrder(node5).toString());
+    }
+
+    private static List<List<Integer>> levelOrder(BinaryTreeInorder.TreeNode root) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        build(map, root, 0);
+        result.addAll(map.values());
+        return result;
+    }
+
+    private static void build(Map<Integer, List<Integer>> map, BinaryTreeInorder.TreeNode node, int level) {
+        List<Integer> list = map.get(level);
+        if(list == null)
+            list = new ArrayList<>();
+
+        list.add(node.val);
+        map.put(level, list);
+
+        if(node.left != null)
+            build(map, node.left, level+1);
+
+        if(node.right != null)
+            build(map, node.right, level+1);
+    }
+
+
+}
